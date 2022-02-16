2fc069f413cfcd8e60853a872854b6ed79cbc79e
ongdyub <ongdyub>
2022-02-16 19:28:57 +0900

    prettier style change

--- src/.prettierrc.js
+++ src/.prettierrc.js
@@ -1,5 +1,8 @@
 module.exports = {
+  printWidth: 120,
   singleQuote: true,
-  trailingComma: 'all',
-  printWidth: 100,
+  useTabs: false,
+  tabWidth: 2,
+  semi: true,
+  bracketSpacing: true,
 };
