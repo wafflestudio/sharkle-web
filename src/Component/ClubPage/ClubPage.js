381bed15d8b2b2b9e9cc6e1c66ec6fb53f1b655d
ongdyub <ongdyub>
2022-02-22 00:10:38 +0900

    css info

--- src/Component/ClubPage/ClubPage.js
+++ src/Component/ClubPage/ClubPage.js
@@ -8,19 +8,19 @@ const ClubPage = () => {
                 <div className="info-main">
                     <div className="info-main-thumb">
                         <div className="info-main-thumb-container">
-                            asdf
+                            그림공간
                         </div>
                     </div>
                     <div className="info-main-name">
-
+                        동아리 이름공간
                     </div>
                 </div>
                 <div className="info-sub">
                     <div className="info-sub-recruit">
-
+                        지원박스
                     </div>
                     <div className="info-sub-tags">
-
+                        태그박스
                     </div>
                 </div>
                 <div className="info-util">
