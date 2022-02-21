c6e79dc35ea7eb0ad90c90eaf81ebce447abc12a
ongdyub <ongdyub>
2022-02-21 23:36:18 +0900

    div skel

---
+++ src/Component/ClubPage/ClubPage.js
@@ -0,0 +1,44 @@
+
+
+const ClubPage = () => {
+
+    return(
+        <div className="container">
+            <div className="info">
+                <div className="info-main">
+                    <div className="info-main-thumb">
+                        <div className="info-main-thumb-container">
+                            asdf
+                        </div>
+                    </div>
+                    <div className="info-main-name">
+
+                    </div>
+                </div>
+                <div className="info-sub">
+                    <div className="info-sub-recruit">
+
+                    </div>
+                    <div className="info-sub-tags">
+
+                    </div>
+                </div>
+                <div className="info-util">
+                    <div className="info-util-alert">
+
+                    </div>
+                </div>
+            </div>
+            <div className="content">
+                <div className="content-menu">
+
+                </div>
+                <div className="content-container">
+
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ClubPage;
