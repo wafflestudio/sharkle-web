09c541314297b910b84f8916b332851cfd5dfabd
HyungGeun-Cho <chris1503@snu.ac.kr>
2022-02-25 14:49:09 +0900

    loginpage + registerpage

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import './App.css';
-import {BrowserRouter, Route, Routes} from "react-router-dom";
-import ClubPage from "./Component/ClubPage/ClubPage";
-import MyPage from "./Component/MyPage/MyPage";
-import Header from "./Component/Header/Header";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import ClubPage from './Component/ClubPage/ClubPage';
+import MyPage from './Component/MyPage/MyPage';
+import Header from './Component/Header/Header';
+import LoginPage from './Component/LoginPage/LoginPage';
+import RegisterPage from './Component/RegisterPage/RegisterPage';
 
 function App() {
   return (
-          <BrowserRouter>
-              <Header/>
-              <Routes>
-                  <Route path="/*" element={<ClubPage />}/>
-                  <Route path="/club" element={<ClubPage />}/>
-                  <Route path="/mypage" element={<MyPage/>}/>
-              </Routes>
-          </BrowserRouter>
-
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/*" element={<ClubPage />} />
+        <Route path="/club" element={<ClubPage />} />
+        <Route path="/mypage" element={<MyPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
