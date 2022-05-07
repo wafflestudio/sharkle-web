1530b1dbd9104c9f4404dcd7d4a2d329ca7637c6
AlongwithKiman <expfunsce@snu.ac.kr>
2022-05-07 20:49:12 +0900

    1

--- src/Component/ClubSearchPage/ClubInfo.js
+++ src/Component/ClubSearchPage/ClubInfo.js
@@ -4,7 +4,9 @@ import { useEffect } from 'react';
 const ClubInfo = ({ club }) => {
   return (
     <div className={styles.club}>
-      <div className={styles['club-icon']}></div>
+      <div className={styles['club-icon']}>
+        <div className={styles['d-day']}>D-16</div>
+      </div>
       <div className={styles['club-name']}>{club.name}</div>
       <div className={styles.tags}>
         {club.tag.split(' ').map((tag) => (
